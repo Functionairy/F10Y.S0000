fa98aee724f1dd4de1403ee6a6eef9ffcc5c075e
davidcoats <david@getsafetycone.com>
2025-04-08 16:53:39 -0700

    Initial commit.

---
+++ source/F10Y.S0000/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace F10Y.S0000
+{
+	/// <summary>
+	/// Organizational entry-point executable.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/F10Y.S0000/Project Plan.md
@@ -0,0 +1,2 @@
+# F10Y.S0000
+Organizational entry-point executable.
